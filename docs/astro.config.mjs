5f9337aa0010575767716245901f22f687df1f27
Luke Turnbull <luke.turnbull@fabra.com>
2025-04-06 12:40:17 +1000

    Update docs
    - Add quick start example
    - Updated links

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
           label: "Guides",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
+            { label: "Example Guide", slug: "guides/quick-start" },
           ],
         },
         {
